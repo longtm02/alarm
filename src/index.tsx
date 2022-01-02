121defc0237bcf36aa0acdee7828fb292ed7c520
longtm <longtm@hblab.vn>
2022-01-02 16:39:53 +0700

    core: implement redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,23 @@
 import { registerRootComponent } from "expo";
 import React from "react";
+import { Provider } from "react-redux";
 import { NativeBaseProvider, extendTheme } from "native-base";
 import App from "./App";
 import { themeConfig } from "./constants/themeConfig";
+import configureStore from "./store";
 
 // extend the theme
 export const theme = extendTheme({ themeConfig });
+//config store
+const store = configureStore();
 
 const Container = () => {
   return (
-    <NativeBaseProvider theme={theme}>
-      <App />
-    </NativeBaseProvider>
+    <Provider store={store}>
+      <NativeBaseProvider theme={theme}>
+        <App />
+      </NativeBaseProvider>
+    </Provider>
   );
 };
 
